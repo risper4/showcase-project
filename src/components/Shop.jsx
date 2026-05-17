1352a431220570d5fdb4f0d875c446bc8ae195e5
Risper <risperwanjikugichia@gmail.com>
2026-05-17 07:37:16 +0300

    Created new smoothies form

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,12 +1,19 @@
 import useFetchData from "../custom hook/useFetchData"
 import ShopList from "./ShopList"
+import AdminPortal from "./AdminPortal"
+
 export default function Shop() {
+
+    function addSmoothies(newData) {
+        setSmoothies((prev) => ([...prev, newData]))
+    }
 
     const {smoothies, loading, setSmoothies} = useFetchData('http://localhost:3001/smoothies')
     
     if(loading) return <p>Loading ...</p>
     return(
         <>
+        <AdminPortal addSmoothies = {addSmoothies}/>
         <ShopList smoothies = {smoothies}/>
         </>
     )
