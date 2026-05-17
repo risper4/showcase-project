aaf50768396fd13322344a10d6d90e6a0bfca156
Risper <risperwanjikugichia@gmail.com>
2026-05-17 08:01:48 +0300

    Added search functionality

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,20 +1,30 @@
 import useFetchData from "../custom hook/useFetchData"
 import ShopList from "./ShopList"
 import AdminPortal from "./AdminPortal"
+import { useState } from "react"
+import Search from "./Search"
 
 export default function Shop() {
+
+    const [search, setSearch] = useState('')
+
 
     function addSmoothies(newData) {
         setSmoothies((prev) => ([...prev, newData]))
     }
 
     const {smoothies, loading, setSmoothies} = useFetchData('http://localhost:3001/smoothies')
+
+    const filteredSmoothies = smoothies.filter((smoothie) => {
+       return smoothie.name.toLowerCase().includes(search.toLowerCase())
+    }) 
     
     if(loading) return <p>Loading ...</p>
     return(
         <>
+        <Search search = {search} setSearch = {setSearch}/>
         <AdminPortal addSmoothies = {addSmoothies}/>
-        <ShopList smoothies = {smoothies}/>
+        <ShopList smoothies = {filteredSmoothies}/>
         </>
     )
 }
