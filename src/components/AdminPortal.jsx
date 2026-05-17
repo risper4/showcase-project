6fb58b3d7f8b82f73061329ba60a53883da3cc99
Risper <risperwanjikugichia@gmail.com>
2026-05-17 15:14:00 +0300

    Added useID

--- src/components/AdminPortal.jsx
+++ src/components/AdminPortal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useId } from "react"
 
 export default function AdminPortal({addSmoothies}) {
 
@@ -44,12 +44,18 @@ export default function AdminPortal({addSmoothies}) {
         setFormData({...formData, [e.target.name] : e.target.value})
     }
 
+    const nameId = useId()
+    const priceId = useId()
+    const ingredientId = useId()
+    const badgeId = useId()
+
+
     return(
         <>
         <form onSubmit={handleSubmit}>
             <input
              type="text"
-             id = '' 
+             id = {nameId}
              name = 'name'
              placeholder="Enter name"
              value = {formData.name}
@@ -57,7 +63,7 @@ export default function AdminPortal({addSmoothies}) {
              />
              <input
              type="number"
-             id = '' 
+             id = {priceId} 
              name = 'price'
              placeholder="Enter price"
              value = {formData.price}
@@ -65,7 +71,7 @@ export default function AdminPortal({addSmoothies}) {
              />
              <input
              type="text"
-             id = '' 
+             id = {ingredientId} 
              name = 'ingredients'
              placeholder="Enter ingredients"
              value = {formData.ingredients}
@@ -73,7 +79,7 @@ export default function AdminPortal({addSmoothies}) {
              />
              <input
              type="text"
-             id = '' 
+             id = {badgeId} 
              name = 'badges'
              placeholder="Enter badge"
              value = {formData.badges}
--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -3,10 +3,10 @@ import ShopCard from "./ShopCard"
 export default function ShopList ({smoothies}) {
 
     return (
-        <>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
         {smoothies.map((smoothie) => {
             <ShopCard key={smoothie.id} smoothie = {smoothie}/>
           })}
-        </>
+        </div>
     )
 }
