34e93bdfdd0f1a8d6c3ff7eeb9d5a524e35f6add
Risper <risperwanjikugichia@gmail.com>
2026-05-18 14:10:08 +0300

    Final fixes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,14 @@
 export default function Search({search, setSearch}) {
 
     return (
-        <>
+        <div>
         <input
+         id="search"
          type="text" 
          placeholder="Search smoothie name"
          value={search || ''}
          onChange={(e) => setSearch(e.target.value)}
-         className="search"
          />
-        </>
+        </div>
     )
 }
